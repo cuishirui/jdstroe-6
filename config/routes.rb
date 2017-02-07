3ee2e7b4786de3fc917688d7ce0d4c856d7f1ecd
cuishirui <344494024@qq.com>
2017-02-07 15:31:47 +0800

    implemet product show

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   namespace :admin do
-    resources :products 
+    resources :products
   end
+
+  resources :products 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

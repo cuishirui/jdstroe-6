291bf4e2dabed36750b033bead78ba826a21ede3
cuishirui <344494024@qq.com>
2017-02-07 15:01:53 +0800

    implement image upload

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ApplicationRecord
  validates  :title,:description, presence: true
  validates :quantity, :price, presence: true
 
- mount_uploader :imgae, ImageUploader
+ mount_uploader :image, ImageUploader
 end
